f09128b00539873867639f48cc6aaf259cfa50f6
Ktari ayman <aymanktari31@gmail.com>
2022-09-01 11:46:56 +0100

    generate token

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const config = process.env;
+
+const verifyToken = (req,res,next) => {
+    let token = req.body.token || req.query.token || req.headers['authorization'];
+    if (!token) {
+        return res.status(401).send("a token required for this request");
+    }
+    try {
+        token = token.replace(/^Bearer\s+/, "");
+        const tokendecoded = jwt.verify(token, config.JWT_SECRET);
+        req.user = tokendecoded;
+      } catch (err) {
+        return res.status(401).send("Invalid Token");
+      }
+    
+      return next();
+}
+
+module.exports = verifyToken;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,15 @@ const auth = require('../controllers/auth/auth');
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
 const { registerValidator, LoginValidator } = require('../helper/valid');
+const verifyToken = require("../middleware/auth")
 
 
 
 router.post('/register',validator.body(registerValidator), auth.controllers.register)
 router.post('/login',validator.body(LoginValidator) , auth.controllers.login)
+router.get('/token',verifyToken , (req, res) => {
+    res.send("token")    
+}
+    )
 
 module.exports = router;
