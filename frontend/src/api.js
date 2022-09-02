8513881cd339bec4ae073ca957f0a01e27999714
Ktari ayman <aymanktari31@gmail.com>
2022-09-02 23:12:42 +0100

    token  + logout function

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
+import { logout } from './helper/auth';
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:8000/api',
     timeout: 1000,
 })
 
+apiClient.interceptors.request.use((config) => {
+  const user = localStorage.getItem('user');
+  if( user){
+    const token= JSON.parse(user).token;
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (err) => {
+    return Promise.reject(err)
+})
 
 export const login = async (data) => {
     try {
@@ -27,3 +38,14 @@ export const login = async (data) => {
       };
     }
   };
+
+  // secure routes 
+
+  const checkResponseCode = (exception) =>{
+    const responseCode = exception?.response?.status;
+    if(responseCode){
+      if(responseCode ===401 || responseCode ===403 ) {
+        logout()
+      }
+    }
+  }
