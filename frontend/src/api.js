f2f8692db48f8c632a1eb299d67b4225feacb821
Ktari ayman <aymanktari31@gmail.com>
2022-09-02 16:54:15 +0100

    apiClient

---
+++ frontend/src/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'https://localhost:8000/api',
+    timeout: 1000,
+})
+
+export const login =async (data) => {
+    try {
+        return await apiClient.post('/auth/login', data)
+    } catch (exception) {
+        return {
+            error:true,
+            exception
+        }
+    }
+}
+
+export const register =async (data) => {
+    try {
+        return await apiClient.post('/auth/register', data)
+    } catch (exception) {
+        return {
+            error:true,
+            exception
+        }
+    }
+}
