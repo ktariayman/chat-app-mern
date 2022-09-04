d81bfb8f205c61e95f8eed6fc5a0b51e112aa676
Ktari ayman <aymanktari31@gmail.com>
2022-09-04 13:02:14 +0100

    fix validate form

--- frontend/src/helper/ValidateForm.js
+++ frontend/src/helper/ValidateForm.js
@@ -12,14 +12,14 @@ export const ValidateRegisterForm = ( {email ,username, password}) =>{
 
 }
 
-const ValidatePassword = (password) => {
+export const ValidatePassword = (password) => {
     return password.length > 7 && password.length <14
 }
-const ValidEmail = (email) => {
+export const ValidEmail = (email) => {
     const EmailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
     return EmailPattern.test(email)
 
 }
-const ValidUsername = (username)=> {
+export const ValidUsername = (username)=> {
     return username.length > 2 && username.length <13
 }
