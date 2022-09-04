bcafc98cccdccd0fdd8e183f4dd2484ec84281be
Ktari ayman <aymanktari31@gmail.com>
2022-09-04 01:19:47 +0100

    add logo to sidebar

--- frontend/src/components/dashboard/sidebar/sidebar.js
+++ frontend/src/components/dashboard/sidebar/sidebar.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import './sidebar.css'
+import Button from '../../../components/buttons/button'
+const imgStyle = {
+  width : "100%",
+  height : "36px",
+  cursor:"pointer",
+}
+const additionalStyles = {
+  margin: "5px",
+    height: "36px",
+}
+
+
 const sidebar = () => {
   return (
-    <div className='sidebar'></div>
+    <div className='sidebar'>
+    <Button  
+            imgUrl="https://seeklogo.com/images/D/discord-icon-new-2021-logo-09772BF096-seeklogo.com.png"
+            imgStyle={imgStyle}
+            additionalStyles={additionalStyles}
+            />
+
+    </div>
   )
 }
 
