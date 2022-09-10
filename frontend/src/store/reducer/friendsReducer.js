40134ec7934a7fbfd73bfe6e0f20487fedf48517
Ktari ayman <aymanktari31@gmail.com>
2022-09-10 12:23:05 +0100

    prepare action & reducer for friends logic

---
+++ frontend/src/store/reducer/friendsReducer.js
@@ -0,0 +1,28 @@
+import { friendsAction } from "../actions/friendActions"
+
+const initState= {
+    friends : [],
+    pendingFriendsInvitation : [],
+    onlineUsers : [],    
+}
+
+const reducer = (state = initState , action) => {
+    switch (action.type) {
+        case friendsAction.SET_PENDING_FRIENDS_INVITATIONS:
+            return {
+                ...state,
+                pendingFriendsInvitation : action.pendingFriendsInvitation
+            }
+            case friendsAction.SET_FRIENDS:
+                return {
+                    ...state,
+                    friends : action.friends
+                }
+            case friendsAction.SET_ONLINE_USERS:
+                    return {
+                        ...state,
+                        onlineUsers : action.onlineUsers
+                    }}
+        }       
+
+export default reducer
