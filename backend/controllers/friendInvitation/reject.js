fd3e572f823eda07b37cf720d4aa8504561a86fe
Ktari ayman <aymanktari31@gmail.com>
2022-09-15 08:01:02 +0100

    reject invitation controller

--- backend/controllers/friendInvitation/reject.js
+++ backend/controllers/friendInvitation/reject.js
@@ -1,6 +1,24 @@
-
+const FriendInvitation = require("../../models/friendInvitation");
+const friendsUpdate  = require ('../../socketHandler/updates/friends')
 
 const reject = async (req, res) => {
-    return res.send('reject')
+
+    try {
+    
+        const {id} = req.body
+        const {userId} = req.user
+        // remove the invitation from friend invitations list
+        const invitationExist = await FriendInvitation.exists({ _id:id})
+        if(invitationExist){
+            await FriendInvitation.findByIdAndDelete(id)
+        }
+            // update pending invitations list
+        friendsUpdate.updateFriendsPendingInvitation(userId)
+        return res.status(200).send('invitations successfully rejected')
+
+    } catch (error) {   
+            console.log(error);
+            return res.status(500).send({ "something wrong , try again" : error });
+    }
 };
 module.exports= reject;
