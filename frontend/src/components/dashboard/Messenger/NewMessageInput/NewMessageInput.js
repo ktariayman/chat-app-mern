0b76788f40e818ec49f49305b5215ccdd6b7111f
Ktari ayman <aymanktari31@gmail.com>
2022-10-22 06:54:11 +0100

    newMessageInput component

--- frontend/src/components/dashboard/Messenger/NewMessageInput/NewMessageInput.js
+++ frontend/src/components/dashboard/Messenger/NewMessageInput/NewMessageInput.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import './NewMessageInput.css'
+import {connect} from 'react-redux'
+import { sendDirectMessage } from '../../../../realtimeCommunication/socketConnection'
+const NewMessageInput = ({chosenChatDetails}) => {
+  const [message, setMessage] = useState('');
 
-const NewMessageInput = () => {
+  const handleMessageValueChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleKeyPressed = (event) => {
+    if (event.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    if(message.length >0) {
+      sendDirectMessage({
+        receiverUserId : chosenChatDetails.id,
+        content:message
+      })
+      setMessage("");
+    }
+
+    }
   return (
-    <div>NewMessageInput</div>
+    <div className='NewMessageInput__MainContainer'>
+      <input 
+        class="NewMessageInput__Input" 
+        placeholder={`write message here to ${chosenChatDetails.name}`}
+        value={message}
+        onChange={handleMessageValueChange}
+        onKeyDown= {handleKeyPressed}
+      /> 
+    </div>
   )
+  }
+const MapStoreStateToProps = ({chat}) => {
+  return {
+    ...chat,
+  }
 }
-
-export default NewMessageInput
+export default connect(MapStoreStateToProps)(NewMessageInput)
