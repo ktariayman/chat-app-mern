3e280c73d22decfc691625efd8e99a953b189495
Ktari ayman <aymanktari31@gmail.com>
2022-10-22 06:56:38 +0100

    add getDirectChatHistory to MessageContent

--- frontend/src/components/dashboard/Messenger/MessangerContent.js
+++ frontend/src/components/dashboard/Messenger/MessangerContent.js
@@ -2,12 +2,15 @@ import React , {useEffect}from 'react'
 import Messages from './Messages/Messages'
 import NewMessageInput from './NewMessageInput/NewMessageInput'
 import './MessangerContent.css'
+import { getDirectChatHistory } from '../../../realtimeCommunication/socketConnection'
 const MessangerContent = ({ chosenChatDetails}) => {
     useEffect(()=>{
-
-    },[])
+      getDirectChatHistory({
+          receiverUserId:  chosenChatDetails.id,
+      })
+    },[chosenChatDetails])
   return (
-    <div class="MessangerContent__wrapper">
+    <div className="MessangerContent__wrapper">
         <Messages />
         <NewMessageInput />
     </div>
