b91557b98948869eeb2e6928326422e3a4937a95
Ktari ayman <aymanktari31@gmail.com>
2022-10-22 06:55:32 +0100

    consume DUMMY_MESSAGES in Messages

--- frontend/src/components/dashboard/Messenger/Messages/Messages.js
+++ frontend/src/components/dashboard/Messenger/Messages/Messages.js
@@ -2,10 +2,24 @@ import React ,{useRef , useEffect}from 'react'
 import MessagesHeader from './MessageHeader'
 import {connect} from 'react-redux'
 import './Messages.css'
+import DUMMY_MESSAGES from './DUMMY_MESSAGES'
+import SingleMessage from './SingleMessage'
 const Messages = ({chosenChatDetails , messages}) => {
   return (
     <div className='messages__mainContainer'>
         <MessagesHeader name={chosenChatDetails?.name}/>
+        {DUMMY_MESSAGES.map((message , index ) => {
+          return <SingleMessage 
+                key={message._id}
+                content={message.content}
+                username={message.author.username}     
+                sameAuthor={message.sameAuthor}      
+                date={message.date}
+                sameDay={message.sameDay}
+                /> 
+        
+        })}
+        
     </div>
   )
 }
